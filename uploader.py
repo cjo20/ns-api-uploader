ffa5fb52a241b8f5079154d0f19defbcdfa2a806
cjo20 <git@cjo20.net>
2015-09-22 13:33:20 +0100

    Add --mmol option, add some basic input checking

--- uploader.py
+++ uploader.py
@@ -4,10 +4,23 @@ import calendar
 import requests
 import hashlib
 
+
+def isANumber(string):
+	try:
+		i = int(string)
+	except (ValueError, TypeError):
+		try:
+			i = float(string)
+		except (ValueError, TypeError):
+			return False
+
+	return True
+
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--api_secret', help="API-SECRET for uploading", required=True)
 parser.add_argument('--base_url', help="Base URL of site", required=True)
 parser.add_argument('--debug', action='store_true')
+parser.add_argument('--mmol', help="Data entered as mmol", action='store_true')
 #parser.add_argument('--data_type', choices=['sgv', 'cal'],default="sgv")
 args = parser.parse_args()
 
@@ -16,14 +29,31 @@ hashed_secret = hashlib.sha1(args.api_secret).hexdigest()
 url = "%s/api/v1/entries" % args.base_url
 print "Uploading information to " + url
 print "Enter 'q' to quit"
+
+prompt = ""
+if args.mmol:
+	prompt = "Enter current BG (mmol/L): "
+else:
+	prompt = "Enter current BG (mg/dL): "
+
 while (True):
-	bg = raw_input("Enter current BG: ")
+
+	bg = raw_input(prompt)
 
 	if (bg == "q"):
 		break
 
-	print("Uploading BG of " + bg)
+	print("Uploading BG " + bg)
 
+	if ( isANumber(bg) == False):
+		print "Invalid value entered: %s" % bg
+		continue
+
+	if (args.mmol):
+		bg = float(bg)
+		bg *= 18.018018
+		bg = int(bg)
+		
 	current_time = time.time()
 	time_struct = time.localtime(current_time)
 
